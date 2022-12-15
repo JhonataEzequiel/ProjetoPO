c1ed508a700b54cbeee7c9be15c3ddfece360733
jhonata <jhonataezequiel@gmail.com>
2022-12-15 08:53:48 -0300

    Finished

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -1,11 +1,10 @@
 from mip import *
 import math
 
-
 primal_limit = -math.inf
 
 
-def branch_and_bound(my_model: Model) -> Tuple[Model, bool]:
+def branch_and_bound(my_model: Model):
     global primal_limit
     status = my_model.optimize()
 
@@ -13,7 +12,6 @@ def branch_and_bound(my_model: Model) -> Tuple[Model, bool]:
         return my_model, False
     if status == OptimizationStatus.NO_SOLUTION_FOUND:
         return my_model, False
-
     if primal_limit > my_model.objective_value:
         return my_model, False
 
@@ -24,14 +22,18 @@ def branch_and_bound(my_model: Model) -> Tuple[Model, bool]:
     min_distance_to_zero_dot_five = math.inf
     variable_closest_to_zero_dot_five = 0
     first_iteration = True
-    
+
     for var in my_model.vars:
+
         if var.x not in [0, 1]:
             none_non_binary_vars = False
+
             if first_iteration:
                 first_iteration = False
                 variable_closest_to_zero_dot_five = var
+
             distance_to_zero_dot_five_now = var.x - 0.5 if var.x - 0.5 >= 0 else 0.5 - var.x
+
             if distance_to_zero_dot_five_now < min_distance_to_zero_dot_five:
                 min_distance_to_zero_dot_five = distance_to_zero_dot_five_now
                 variable_closest_to_zero_dot_five = var
@@ -39,35 +41,21 @@ def branch_and_bound(my_model: Model) -> Tuple[Model, bool]:
     if none_non_binary_vars:
         return my_model, True
 
+    print(f"\nVariable chosen to receive the constraints: {variable_closest_to_zero_dot_five.name}")
+    print(f"Value of the variable: {variable_closest_to_zero_dot_five.x}\n")
+
     left_branch += variable_closest_to_zero_dot_five == 0
+    right_branch += variable_closest_to_zero_dot_five == 1
     generated_model_and_status_for_left_side = branch_and_bound(left_branch)
 
-    right_branch += variable_closest_to_zero_dot_five == 1
-    generated_model_and_status_for_right_side = branch_and_bound(right_branch)
-
-    if generated_model_and_status_for_left_side[1]:
+    if generated_model_and_status_for_left_side is not None and generated_model_and_status_for_left_side[1]:
         if generated_model_and_status_for_left_side[0].objective_value > primal_limit:
             primal_limit = generated_model_and_status_for_left_side[0].objective_value
-    if generated_model_and_status_for_right_side[1]:
+
+    generated_model_and_status_for_right_side = branch_and_bound(right_branch)
+
+    if generated_model_and_status_for_right_side is not None and generated_model_and_status_for_right_side[1]:
         if generated_model_and_status_for_right_side[0].objective_value > primal_limit:
             primal_limit = generated_model_and_status_for_right_side[0].objective_value
 
-    if generated_model_and_status_for_right_side[1] and generated_model_and_status_for_left_side[1]:
-
-        if generated_model_and_status_for_right_side[0].objective_value > generated_model_and_status_for_left_side[0].objective_value:
-            if generated_model_and_status_for_right_side[0].objective_value >= primal_limit:
-                return generated_model_and_status_for_right_side[0], generated_model_and_status_for_right_side[1]
-
-        else:
-            if generated_model_and_status_for_left_side[0].objective_value >= primal_limit:
-                return generated_model_and_status_for_left_side[0], generated_model_and_status_for_left_side[1]
-
-    elif generated_model_and_status_for_right_side[1]:
-        if generated_model_and_status_for_right_side[0].objective_value >= primal_limit:
-            return generated_model_and_status_for_right_side[0], generated_model_and_status_for_right_side[1]
-
-    elif generated_model_and_status_for_left_side[1]:
-        if generated_model_and_status_for_left_side[0].objective_value >= primal_limit:
-            return generated_model_and_status_for_left_side[0], generated_model_and_status_for_left_side[1]
-
-    return my_model, False
+    return
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import bnb
-from branch_and_bound import *
+import branch_and_bound
+from mip import *
+
 
 if __name__ == '__main__':
-    with open('input.txt') as f:
+
+    with open('input-1.txt') as f:
         lines = f.readlines()
 
     quantity_of_vars_and_restrictions = [int(lines[0].split(' ')[0]), int(lines[0].split(' ')[1].strip())]
@@ -15,10 +17,10 @@ if __name__ == '__main__':
     ]
     right_side = [coefficients[-1] for coefficients in coefficients_of_restrictions]
     for i in range(len(coefficients_of_restrictions)):
-        coefficients_of_restrictions[i].remove(right_side[i])
+        coefficients_of_restrictions[i] = coefficients_of_restrictions[i][:-1]
 
     model = Model(sense=MAXIMIZE, solver_name=CBC)
-    variables = [model.add_var() for _ in range(quantity_of_vars_and_restrictions[0])]
+    variables = [model.add_var(var_type=CONTINUOUS, lb=0, ub=1) for _ in range(quantity_of_vars_and_restrictions[0])]
     model.objective = xsum(
             coefficients_in_the_objective_func[i] * variables[i]
             for i in range(len(coefficients_in_the_objective_func))
@@ -29,11 +31,10 @@ if __name__ == '__main__':
         model += xsum(coefficients_of_restriction[i] * variables[i] for i in range(len(coefficients_of_restriction))) \
                  <= right_side[j]
         j += 1
+
     model.write('model.lp')
     with open('model.lp') as modelo:
         print(modelo.read())
-    my_bnb = branch_and_bound(model)[0].objective_value
-    egidio = bnb.branch_and_bound(model)[0].objective_value
-    print(f"Meu BNB: {my_bnb}")
-    print(f"BNB de Egídio: {egidio}")
+    branch_and_bound.branch_and_bound(model)
+    print(branch_and_bound.primal_limit)
     f.close()
